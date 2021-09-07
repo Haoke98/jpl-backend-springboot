9847572298141f9f65fa998ffdf2c7bcebb8a718
Sadam <1903249375@qq.com>
2021-09-07 17:27:04 +0800

    初始化了SpringBoot并初次提交

---
+++ src/main/java/bup/edu/cn/BeautifulJapanese/BeautifulJapaneseBackendApplication.java
@@ -0,0 +1,13 @@
+package bup.edu.cn.BeautifulJapanese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulJapaneseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulJapaneseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bup/edu/cn/BeautifulJapanese/BeautifulJapaneseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bup.edu.cn.BeautifulJapanese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulJapaneseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
